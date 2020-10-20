bb0b3655644a99de1bb13de0d0d8a22bfde81553
JD Alexander Kemp <jdakemp@gmail.com>
2020-10-20 15:53:06 -0400

    Passport username and password strategy

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -47,6 +49,9 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -54,44 +59,36 @@ function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+  } else {
+    next();
   }
-  else {
-    if (req.seesion.user === 'authenticated') {
-      next();
-    } else {
-      var err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-    }
-  }
-}
 
-app.use(auth);
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use(auth);
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dishes', dishRouter);
-app.use('/promotions', promotionRouter);
-app.use('/leaders', leaderRouter);
+  app.use('/', indexRouter);
+  app.use('/users', usersRouter);
+  app.use('/dishes', dishRouter);
+  app.use('/promotions', promotionRouter);
+  app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+}
 module.exports = app;
